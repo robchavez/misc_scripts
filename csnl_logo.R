d0ea2c2f9f165b2d327790982e5a7de7236cb15b
Rob Chavez <robert.s.chavez@gmail.com>
2023-03-23 11:52:22 -0700

    bold

--- csnl_logo.R
+++ csnl_logo.R
@@ -12,31 +12,30 @@ cart <- radial$transform(rad$r, rad$a)
 
 # Have a look 
 ggplot() +
-  geom_path(aes(x = x, y = y), data = cart, color = 'forestgreen', size=1) +
-  geom_curve(aes(x = 1.2, y = 0, xend = 5, yend = 1.1), curvature = .28, color='forestgreen', size=1) +
-  geom_curve(aes(x = 5, y = 1.1, xend = 3.6, yend = 6), curvature = .5, color='forestgreen', size=1) +
+  geom_path(aes(x = x, y = y), data = cart, color = 'forestgreen', size=2) +
+  geom_curve(aes(x = 1.25, y = 0, xend = 5, yend = 1.1), curvature = .28, color='forestgreen', size=2, lineend = 'round', ncp = 4500) +
+  geom_curve(aes(x = 5, y = 1.1, xend = 3.6, yend = 6), curvature = .5, color='forestgreen', size=2, lineend = 'round', ncp = 4500) +
   
   #central
-  geom_curve(aes(x = 0, y = 6.4, xend = 1, yend = 2.5), curvature = -.3, color='forestgreen') +
-  geom_curve(aes(x = -.8, y = 6.2, xend = 0.2, yend = 2.5), curvature = -.3, color='forestgreen') +
-
-  #STS
-  geom_curve(aes(x = -3.5, y = 0, xend = 0, yend = -2), curvature = .3, color='forestgreen') +
-  geom_curve(aes(x = -3, y = 1, xend = 0, yend = -1), curvature = .3, color='forestgreen') +
-
-  #parietal
-  geom_curve(aes(x = -3, y = 3, xend = -2, yend = 4), curvature = -.3, color='forestgreen') +
+  geom_curve(aes(x = 0, y = 6.4, xend = 1, yend = 2.5), curvature = -.3, size=1, color='forestgreen') +
+  geom_curve(aes(x = -.8, y = 6.2, xend = 0.2, yend = 2.5), curvature = -.3, size=1, color='forestgreen') +
   
-  geom_curve(aes(x = -.3, y = .5, xend = -.4, yend = 2), curvature = -.3, color='forestgreen') +
-  geom_curve(aes(x = -.3, y = .5, xend = -1.5, yend = 1), curvature = .3, color='forestgreen') +
-
+  #STS
+  geom_curve(aes(x = -3.5, y = 0, xend = 0, yend = -2), curvature = .3, size=1, color='forestgreen') +
+  geom_curve(aes(x = -3, y = 1, xend = 0, yend = -1), curvature = .3, size=1, color='forestgreen') +
+  
+  #parietal
+  geom_curve(aes(x = -3, y = 3, xend = -2, yend = 4), curvature = -.3, size=1, color='forestgreen') +
+  
+  geom_curve(aes(x = -.3, y = .5, xend = -.4, yend = 2), curvature = -.3, size=1, color='forestgreen') +
+  geom_curve(aes(x = -.3, y = .5, xend = -1.5, yend = 1), curvature = .3, size=1, color='forestgreen') +
+  
   #front
-  geom_curve(aes(x = 2, y = .5, xend = 4, yend = 1), curvature = .3, color='forestgreen') +
-  geom_curve(aes(x = 4.5, y = 4, xend = 3, yend = 3), curvature = .3, color='forestgreen') +
-  geom_curve(aes(x = 4.5, y = 4, xend = 3, yend = 4.8), curvature = -.2, color='forestgreen') +
+  geom_curve(aes(x = 2, y = .5, xend = 4, yend = 1), curvature = .3, size=1, color='forestgreen') +
+  geom_curve(aes(x = 4.5, y = 4, xend = 3, yend = 3), curvature = .3, size=1, color='forestgreen') +
+  geom_curve(aes(x = 4.5, y = 4, xend = 3, yend = 4.8), curvature = -.2, size=1, color='forestgreen') +
   coord_cartesian(ylim = c(-3.5,7), xlim = c(-5.5,6)) +
   annotate("text", x =4 , y = -1.5, label = "The Computational \nSocial Neuroscience Lab", size = 5.5) +
   annotate("text", x =4 , y = -2.5, label = "at University of Oregon", color = "forestgreen",  size = 3.5) +
   theme_void()
-  
   
